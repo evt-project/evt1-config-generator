b068956762e2618105ad4a2b8f23edb57cc85a4e
ChiaraDipi <dipi.chiara@gmail.com>
2019-11-12 09:33:43 +0100

    Set default configs in EVT config service

--- src/app/services/evt-config.service.ts
+++ src/app/services/evt-config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { EditionMainData, EditionTools, AdvancedConfigs, EVT1Config } from '../evt-config.models';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,92 +8,7 @@ import { EditionMainData, EditionTools, AdvancedConfigs, EVT1Config } from '../e
 export class EvtConfigService {
   uploadedConfig: EventEmitter<any> = new EventEmitter();
 
-  defaultConfigs: EVT1Config = {
-    mainData: {
-      // GLOBAL
-      index_title: 'CodexViewer',
-      badge: true,
-      badge_text: 'DIGITAL',
-      webSite: '',
-      welcomeMsg: '',
-      // EDITION LEVELS
-      edition_array: [{
-        label: 'Diplomatic',
-        value: 'Diplomatic',
-        prefix: 'dipl',
-        visible: true
-      }, {
-        label: 'Interpretative',
-        value: 'Interpretative',
-        prefix: 'interp',
-        visible: true
-      }],
-      edition_level_selector: true,
-      imageExt: 'jpg',
-      fb_thumb: 'thumb_fb.jpg'
-    },
-    tools: {
-      // ADDITIONAL CONTENTS
-      translation: false,
-      // TOOLS
-      txtimg_link_button: true,
-      hs_button: true,
-      mag_button: true,
-      thumbs_button: true,
-      pp_selector_pos: 'right',
-      pp_selector_doc_grouping: true,
-      pp_selector_doc_tooltip: true,
-      document_navigation: true,
-      // TOOLS - SEARCH
-      search: true,
-      virtual_keyboard_search: false,
-      // TOOLS - LISTS
-      list_person: true,
-      list_place: true,
-      list_org: true,
-      list_term: false,
-      list_gloss: false,
-      list_doc: false,
-      lists: [], // ['persName', 'placeName', 'roleName', 'measure', 'date', 'orgName'],
-      // TOOLS - VIEWS
-      image_frame: true,
-      double_view: true,
-      // DEFAULT CONTENT SEEN
-      left_frame_default_content: 'image',
-      right_frame_default_content: 'info',
-      // PREFATORY MATTER
-      regesto: false,
-      frontInfo: true,
-      msDesc: true,
-      headerInfo: true,
-      bibliography: true,
-      // VISCOLL
-      viscoll_button: false,
-      viscoll_scheme_path: '',
-      viscoll_image_list_path: '',
-      // BOTTOM BAR
-      bottom_navbar: false,
-      bottom_navbar_initial_status: 'closed',
-      // POETRY
-      prose_verses_toggler: false,
-      lang_tooltip: false,
-    },
-    advanced: {
-      // ADVANCED
-      defaulTextLabel: true,
-      mainPrefix: '.',
-      filePrefix: '../../..',
-      dataPrefix: '../..',
-      ed_content: 'if(//tei:text/tei:group) then(//tei:text/tei:group/name()) else ( //tei:text/name() )',
-      // tslint:disable-next-line: max-line-length
-      start_split: `if(//tei:text/tei:group) then(//tei:text/tei:group/name()) else(if(count(//tei:body/tei:div[@subtype='edition_text'])>1) then(//tei:body/tei:div[@subtype='edition_text']/name()) else(//tei:text/tei:body/name() ))`,
-      start_split_depth: '//node()[name()=$start_split]/count(ancestor-or-self::node())',
-      max_depth: `max(((max(//tei:pb/count(ancestor-or-self::node())) - //node()[name()=$start_split]/count(ancestor-or-self::node())),
-                    (max(//tei:lb/count(ancestor-or-self::node())) - //node()[name()=$start_split]/count(ancestor-or-self::node())),
-                    (max(//tei:cb/count(ancestor-or-self::node())) - //node()[name()=$start_split]/count(ancestor-or-self::node()))))`,
-      evtTxt: ''
-    }
-  };
+  defaultConfigs: EVT1Config;
   configs: EVT1Config;
   defaultDataFolder = 'data';
 
@@ -105,48 +21,8 @@ export class EvtConfigService {
   };
 
   constructor() {
-    this.defaultConfigs.mainData.welcomeMsg = `<div>
-      <p class="title main">Welcome to an edition created with EVT!</p>
-    </div>
-    <div>This archive includes a few examples of editions created using EVT,
-      by default you are showed a small excerpt of the
-      <a href="http://pelavicino.labcd.unipi.it/" target="blank">
-        Codice Pelavicino Digitale edition</a>.
-    </div>
-    <div>It is recommended to go to full screen mode so that all available screen space
-    is used to show the manuscript images and the transcription text.</div>
-    <div>For more information refer to the EVT Manual in the "doc" folder.
-      If you have any suggestions or spot an error/bug please contact us at
-    <a href="mailto:evt.developers@gmail.com">evt.developers@gmail.com</a></div>`;
-
-    this.defaultConfigs.advanced.evtTxt = `<p>EVT (Edition Visualization Technology) is a software for creating and browsing
-      digital editions of manuscripts based on text encoded according to the TEI XML schemas and Guidelines.
-      This tool was born as part of the DVB (Digital Vercelli Book) project in order to allow the creation
-      of a digital edition of the Vercelli Book, a parchment codex of the late tenth century, now preserved
-      in the Archivio e Biblioteca Capitolare of Vercelli and regarded as one of the four most important
-      manuscripts of the Anglo-Saxon period as regards the transmission of poetic texts in the Old English language.
-    </p>
-    <p>To ensure that it will be working on all the most recent web browsers, and for as long as possible on the World
-      Wide Web itself, EVT is built on open and standard web technologies such as HTML, CSS and JavaScript. Specific
-      features, such as the magnifying lens, are entrusted to jQuery plugins, again chosen among the open source and
-      best supported ones to reduce the risk of future incompatibilities. The general architecture of the software,
-      in any case, is modular, so that any component which may cause trouble or turn out to be not completely up to
-      the task can be replaced easily.</p>
-    <p>For more information about how to use and/or customize EVT please refer to the EVT Manual included in the
-      archive you downloaded, in the "doc" folder.</p>
-    <p>EVT is used in the following projects:</p>
-    <ul>
-      <li><a href="http://pelavicino.labcd.unipi.it/" target="blank">Codice Pelavicino Digitale</a></li>
-      <li><a href="http://vbd.humnet.unipi.it/" target="blank">Vercelli Book Digitale</a></li>
-    </ul>
-    <p>EVT has a home page in the <a href="https://sourceforge.net/p/evt-project/" target="blank">SourceForge</a> repository,
-      but development is done on Gitlab and Github: if you are interested in learning more about EVT and/or
-      in adapting it to your specific needs please contact the project Director, Roberto Rosselli Del Turco
-      <a href="mailto:roberto.rossellidelturco@gmail.com" target="_top">roberto.rossellidelturco@gmail.com</a>.</p>
-    <p>If you have any suggestions or spot an error/bug please contact us at 
-    <a href="mailto:evt.developers@gmail.com">evt.developers@gmail.com</a></p>`;
-
-    this.configs = JSON.parse(JSON.stringify(this.defaultConfigs));
+    this.setDefaults();
+    this.configs = { ...this.defaultConfigs };
   }
 
   getUploadedConfigEmitter() {
@@ -176,7 +52,7 @@ export class EvtConfigService {
                     label: subChild.innerHTML,
                     value: subChild.innerHTML,
                     prefix,
-                    visible: true
+                    visible: subChild.innerHTML !== ''
                   });
                 } else if (paramName === 'lists') {
                   const items = Array.from(subChild.children).map(i => {
@@ -280,7 +156,10 @@ export class EvtConfigService {
     return this.configs;
   }
 
-  getProperty(sectionName: string, propertyName: string) {
+  getProperty(propertyName: string, sectionName?: string) {
+    if (!sectionName) {
+      sectionName = this.findPropertySection(propertyName);
+    }
     return this.configs[sectionName][propertyName];
   }
 
@@ -310,6 +189,7 @@ export class EvtConfigService {
       }
     }
   }
+
   updateEditionLevelLabel(editionLevelToEdit: string, label: string) {
     for (const edition of this.configs.mainData.edition_array) {
       if (edition.value === editionLevelToEdit) {
@@ -317,6 +197,7 @@ export class EvtConfigService {
       }
     }
   }
+
   updateEditionLevelPrefix(editionLevelToEdit: string, prefix: string) {
     for (const edition of this.configs.mainData.edition_array) {
       if (edition.value === editionLevelToEdit) {
@@ -333,5 +214,135 @@ export class EvtConfigService {
         }
       }
     }
+  }
+
+  private setDefaults() {
+    this.defaultConfigs = {
+      mainData: {
+        // GLOBAL
+        index_title: 'CodexViewer',
+        badge: true,
+        badge_text: 'DIGITAL',
+        webSite: '',
+        welcomeMsg: '',
+        // EDITION LEVELS
+        edition_array: [{
+          label: 'Diplomatic',
+          value: 'Diplomatic',
+          prefix: 'dipl',
+          visible: true
+        }, {
+          label: 'Interpretative',
+          value: 'Interpretative',
+          prefix: 'interp',
+          visible: true
+        }],
+        edition_level_selector: true,
+        imageExt: 'jpg',
+        fb_thumb: 'thumb_fb.jpg'
+      },
+      tools: {
+        // ADDITIONAL CONTENTS
+        translation: false,
+        // TOOLS
+        txtimg_link_button: true,
+        hs_button: true,
+        mag_button: true,
+        thumbs_button: true,
+        pp_selector_pos: 'right',
+        pp_selector_doc_grouping: true,
+        pp_selector_doc_tooltip: true,
+        document_navigation: true,
+        // TOOLS - SEARCH
+        search: true,
+        virtual_keyboard_search: false,
+        // TOOLS - LISTS
+        list_person: true,
+        list_place: true,
+        list_org: true,
+        list_term: false,
+        list_gloss: false,
+        list_doc: false,
+        lists: [], // ['persName', 'placeName', 'roleName', 'measure', 'date', 'orgName'],
+        // TOOLS - VIEWS
+        image_frame: true,
+        double_view: true,
+        // DEFAULT CONTENT SEEN
+        left_frame_default_content: 'image',
+        right_frame_default_content: 'info',
+        // PREFATORY MATTER
+        regesto: false,
+        frontInfo: true,
+        msDesc: true,
+        headerInfo: true,
+        bibliography: true,
+        // VISCOLL
+        viscoll_button: false,
+        viscoll_scheme_path: '',
+        viscoll_image_list_path: '',
+        // BOTTOM BAR
+        bottom_navbar: false,
+        bottom_navbar_initial_status: 'closed',
+        // POETRY
+        prose_verses_toggler: false,
+        lang_tooltip: false,
+      },
+      advanced: {
+        // ADVANCED
+        defaulTextLabel: true,
+        mainPrefix: '.',
+        filePrefix: '../../..',
+        dataPrefix: '../..',
+        ed_content: 'if(//tei:text/tei:group) then(//tei:text/tei:group/name()) else ( //tei:text/name() )',
+        // tslint:disable-next-line: max-line-length
+        start_split: `if(//tei:text/tei:group) then(//tei:text/tei:group/name()) else(if(count(//tei:body/tei:div[@subtype='edition_text'])>1) then(//tei:body/tei:div[@subtype='edition_text']/name()) else(//tei:text/tei:body/name() ))`,
+        start_split_depth: '//node()[name()=$start_split]/count(ancestor-or-self::node())',
+        max_depth: `max(((max(//tei:pb/count(ancestor-or-self::node())) - //node()[name()=$start_split]/count(ancestor-or-self::node())),
+                      (max(//tei:lb/count(ancestor-or-self::node())) - //node()[name()=$start_split]/count(ancestor-or-self::node())),
+                      (max(//tei:cb/count(ancestor-or-self::node())) - //node()[name()=$start_split]/count(ancestor-or-self::node()))))`,
+        evtTxt: ''
+      }
+    };
+
+    this.defaultConfigs.mainData.welcomeMsg = `<div>
+      <p class="title main">Welcome to an edition created with EVT!</p>
+    </div>
+    <div>This archive includes a few examples of editions created using EVT,
+      by default you are showed a small excerpt of the
+      <a href="http://pelavicino.labcd.unipi.it/" target="blank">
+        Codice Pelavicino Digitale edition</a>.
+    </div>
+    <div>It is recommended to go to full screen mode so that all available screen space
+    is used to show the manuscript images and the transcription text.</div>
+    <div>For more information refer to the EVT Manual in the "doc" folder.
+      If you have any suggestions or spot an error/bug please contact us at
+    <a href="mailto:evt.developers@gmail.com">evt.developers@gmail.com</a></div>`;
+
+    this.defaultConfigs.advanced.evtTxt = `<p>EVT (Edition Visualization Technology) is a software for creating and browsing
+      digital editions of manuscripts based on text encoded according to the TEI XML schemas and Guidelines.
+      This tool was born as part of the DVB (Digital Vercelli Book) project in order to allow the creation of
+      a digital edition of the Vercelli Book, a parchment codex of the late tenth century,
+      now preserved in the Archivio e Biblioteca Capitolare of Vercelli and regarded as one of the four most
+      important manuscripts of the Anglo-Saxon period as regards the transmission of poetic texts in the Old English language.
+    </p>
+    <p>To ensure that it will be working on all the most recent web browsers, and for as long as possible on the World
+      Wide Web itself, EVT is built on open and standard web technologies such as HTML, CSS and JavaScript. Specific
+      features, such as the magnifying lens, are entrusted to jQuery plugins, again chosen among the open source and
+      best supported ones to reduce the risk of future incompatibilities. The general architecture of the software,
+      in any case, is modular, so that any component which may cause trouble or turn out to be not completely up to
+      the task can be replaced easily.</p>
+    <p>For more information about how to use and/or customize EVT please refer to the EVT Manual included in the
+      archive you downloaded, in the "doc" folder.</p>
+    <p>EVT is used in the following projects:</p>
+    <ul>
+      <li><a href="http://pelavicino.labcd.unipi.it/" target="blank">Codice Pelavicino Digitale</a></li>
+      <li><a href="http://vbd.humnet.unipi.it/" target="blank">Vercelli Book Digitale</a></li>
+    </ul>
+    <p>EVT has a home page in the <a href="https://sourceforge.net/p/evt-project/" target="blank">SourceForge</a> repository,
+      but development is done on Gitlab and Github: if you are interested in learning more about EVT and/or
+      in adapting it to your specific needs please contact the project Director, Roberto Rosselli Del Turco
+      <a href="mailto:roberto.rossellidelturco@gmail.com" target="_top">roberto.rossellidelturco@gmail.com</a>.</p>
+    <p>If you have any suggestions or spot an error/bug please contact us at
+    <a href="mailto:evt.developers@gmail.com">evt.developers@gmail.com</a></p>`;
   }
 }
